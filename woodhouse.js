16ebadfda78b1ab46be8134f17b39d28c697b981
Algram <aliasgram@gmail.com>
2016-08-11 22:28:38 +0200

    Updated syntax to be conform with eslint

--- woodhouse.js
+++ woodhouse.js
@@ -1,27 +1,26 @@
-var Bots = require('node-telegram-bot-api');
-var config = require('./config.json');
+const Bot = require('node-telegram-bot-api');
+const config = require('./config.json');
 
-var TOKEN = config.key;
-var MESSAGE_STR = process.argv[2];
+const TOKEN = config.key;
+// const MESSAGE_STR = process.argv[2];
 
-var wh = new Bot(TOKEN, {polling: true});
+const WH = new Bot(TOKEN, { polling: true });
 
 // Matches /shutdown [whatever]
-wh.onText(/\/shutdown (.+)/, function (msg, match) {
-  console.log('asd');
-  var fromId = msg.from.id;
+WH.onText(/\/shutdown (.+)/, (msg, match) => {
+  const fromId = msg.from.id;
 
   switch (match[1]) {
-    case 'rpizero':
-      var resp = match[1];
-      wh.sendMessage(fromId, "Shutting down Raspberry Pi Zero..");
-
+    case 'rpizero': {
+      WH.sendMessage(fromId, 'Shutting down Raspberry Pi Zero..');
       break;
-    default:
-      wh.sendMessage(fromId, "Sorry, I don't know that machine");
+    }
+    default: {
+      WH.sendMessage(fromId, "Sorry, I don't know that machine");
+    }
   }
 });
 
-/*setTimeout(function() {
+/* setTimeout(function() {
     process.exit();
 }, 10000);*/
