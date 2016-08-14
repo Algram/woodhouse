1cceef8acc5e7e5cd312ae83556466df9bdb6beb
Algram <aliasgram@gmail.com>
2016-08-15 00:24:08 +0200

    Added youtube wrapper

--- youtube.js
+++ youtube.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const google = require('googleapis');
 const youtube = google.youtube('v3');
+const youtubeDl = google.youtube('youtube-dl');
 const config = require('./config.json');
 
 function searchByVideoName(query, cb) {
@@ -26,6 +29,24 @@ function searchByVideoName(query, cb) {
   });
 }
 
+function download(videoId, options) {
+  const video = youtubeDl(`http://www.youtube.com/watch?v=${videoId}`,
+  // Optional arguments passed to youtube-dl.
+  ['--extract-audio', 'audio-format=mp3'],
+  // Additional options can be given for calling `child_process.execFile()`.
+  { cwd: __dirname });
+
+  // Will be called when the download starts.
+  video.on('info', info => {
+    console.log('Download started');
+    console.log('filename:', info.filename);
+    console.log('size:', info.size);
+  });
+
+  video.pipe(path.join(options.downloadDir, fs.createWriteStream(`${videoId}.mp3`)));
+}
+
 module.exports = {
-  searchByVideoName: searchByVideoName
+  searchByVideoName: searchByVideoName,
+  download: download
 };
