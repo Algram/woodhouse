42b49e60012bea1f0d21f8edd744fec52bcd2256
Algram <aliasgram@gmail.com>
2016-09-02 19:06:34 +0200

    Copied custom md5 library from fritzbox (why did they do that?)

--- fritzbox.js
+++ fritzbox.js
@@ -1,12 +1,25 @@
-const request = require('request');
-const FritzBoxAPI = require('fritz-box').default;
-
 const config = require('./config.json');
+const md5 = require('./lib/fritzbox_md5');
+const request = require('request');
+const cheerio = require('cheerio');
 
-function showHome() {
-  const fb = new FritzBoxAPI({
-    username: 'admin',
-    password: config.fritzbox,
-    host: 'fritz.box'
+
+
+function getActiveDevices() {
+  request('http://fritz.box', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const challengePosStart = body.indexOf('"challenge":') + 14;
+      const challengePosEnd = body.indexOf(',', challengePosStart) - 1;
+      const challenge = body.substring(challengePosStart, challengePosEnd);
+
+      console.log(challenge);
+      console.log(md5.hex('test'));
+
+      //let $ = cheerio.load(body);
+
+
+    }
   });
 }
+
+getActiveDevices();
