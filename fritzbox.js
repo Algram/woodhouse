44fe3e824b2624bd3337a11103c40f305bef7812
Algram <aliasgram@gmail.com>
2016-09-02 20:36:33 +0200

    Switched to crypto and removed custom md5 lib

--- fritzbox.js
+++ fritzbox.js
@@ -1,7 +1,6 @@
 const config = require('./config.json');
-const md5 = require('./lib/fritzbox_md5');
+const crypto = require('crypto');
 const request = require('request');
-const cheerio = require('cheerio');
 
 
 function getActiveDevices() {
@@ -11,12 +10,43 @@ function getActiveDevices() {
       const challengePosEnd = body.indexOf(',', challengePosStart) - 1;
       const challenge = body.substring(challengePosStart, challengePosEnd);
 
-      let paramResponse = `${challenge} - ${config.fritzbox.password}`;
-      paramResponse = `${challenge} - ${md5.hex(paramResponse)}`;
+      let paramResponse = `${challenge}-${config.fritzbox.password}`;
 
-      console.log(paramResponse);
+      // Hash paramResponse
+      const md5 = crypto.createHash('md5');
+      md5.update(paramResponse, 'ucs2'); // 16 - Unicode
+      const digest = md5.digest('hex');
+
+      paramResponse = `${challenge}-${digest}`;
+
+      request.post( {url:'http://fritz.box', form: {
+        response: paramResponse,
+        lp: 'netDev',
+        username: ''
+      } }, (err, httpResponse, body) => {
+        const sessionIdPosStart = body.indexOf('"sid":') + 8;
+        const sessionId = body.substring(sessionIdPosStart, sessionIdPosStart + 16);
+
+        console.log(sessionIdPosStart, sessionId);
+
+        request.post({ url: 'http://fritz.box/data.lua',
+        form: {
+          sid: sessionId,
+          lang: 'en',
+          page: 'netDev',
+          type: 'all',
+          xhr: 1,
+          no_sidrenew: ''
+        } }, (err,httpResponse,body) => {
+          console.log(body);
+        });
+      });
     }
   });
+}
+
+function getFritzBoxData() {
+
 }
 
 getActiveDevices();
