48da211d689534e3bc7894cc133dc18aa73cdef6
Algram <aliasgram@gmail.com>
2016-09-09 21:07:22 +0200

    Added authenticate() function

--- spotify.js
+++ spotify.js
@@ -6,29 +6,38 @@ const API = new SpotifyWebApi({
   clientSecret: config.spotify.secret
 });
 
-// Retrieve an access token.
-API.clientCredentialsGrant()
-  .then(data => {
-    // Save the access token so that it's used in future calls
-    API.setAccessToken(data.body.access_token);
-  }, err => {
-    console.log('Something went wrong when retrieving an access token', err);
+function authenticate() {
+  return new Promise((resolve, reject) => {
+    // Retrieve an access token.
+    API.clientCredentialsGrant()
+    .then(data => {
+      // Save the access token so that it's used in future calls
+      API.setAccessToken(data.body.access_token);
+      resolve();
+    }, err => {
+      console.log('Something went wrong when retrieving an access token', err);
+      reject(err);
+    });
   });
+}
 
 function getSongsFromSharePlaylist() {
   return new Promise((resolve, reject) => {
-    API.getPlaylist(config.spotify.username, config.spotify.syncPlaylistId)
-    .then(data => {
-      const tracks = [];
-      for (const item of data.body.tracks.items) {
-        const fullName = `${item.track.artists[0].name} - ${item.track.name}`;
-        tracks.push(fullName);
-      }
+    authenticate()
+    .then(() => {
+      API.getPlaylist(config.spotify.username, config.spotify.syncPlaylistId)
+      .then(data => {
+        const tracks = [];
+        for (const item of data.body.tracks.items) {
+          const fullName = `${item.track.artists[0].name} - ${item.track.name}`;
+          tracks.push(fullName);
+        }
 
-      resolve(tracks);
-    }, err => {
-      console.log('Something went wrong!', err);
-      reject("Couldn't get tracks");
+        resolve(tracks);
+      }, err => {
+        console.log('Something went wrong!', err);
+        reject("Couldn't get tracks");
+      });
     });
   });
 }
